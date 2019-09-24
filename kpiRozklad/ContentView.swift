0f1dc548f214bf712b0b78ba093c05c90092e95b
ddanilyuk <danis.danilyuk@gmail.com>
2019-09-24 22:52:17 +0300

    Initial Commit

---
+++ kpiRozklad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kpiRozklad
+//
+//  Created by Denis on 9/24/19.
+//  Copyright © 2019 Denis Danilyuk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
